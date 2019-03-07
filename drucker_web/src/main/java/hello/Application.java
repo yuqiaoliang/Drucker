62f598c904a3f29473c8243d32ce5588aee66533
Tianrui Zhang <tz85@duke.edu>
2019-03-07 12:42:28 -0500

    clean up

--- drucker_web/src/main/java/hello/Application.java
+++ drucker_web/src/main/java/hello/Application.java
@@ -1,13 +1,14 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        run(Application.class, args);
     }
 
 }
