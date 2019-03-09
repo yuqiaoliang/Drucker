a88e63cf1e5a2f5b12efbf04b9db4bbaa0f91dfe
Vanessa Sun <vanessasun@Vanessas-MacBook-Air.local>
2019-03-08 22:22:39 -0500

    remote database access completed

--- drucker_web/src/main/java/model/Account.java
+++ drucker_web/src/main/java/model/Account.java
@@ -1,47 +1,45 @@
 package model;
 import java.sql.*;
 import java.util.HashMap;
+import org.json.simple.JSONObject;
 
 public class Account {
 
    // private HashMap<String, String> users;
 
     public Account() {
-        //create connection to mysql db
+        //create connection to mysql db on vm
         try {
-          Class.forName("com.mysql.cj.jdbc.Driver");     //加载MYSQL JDBC驱动程序
-          //Class.forName("org.gjt.mm.mysql.Driver");
-         System.out.println("Success loading Mysql Driver!");
+          Class.forName("com.mysql.cj.jdbc.Driver");
+          System.out.println("Success loading Mysql Driver!");
         }
         catch (Exception e) {
           System.out.print("Error loading Mysql Driver!");
           e.printStackTrace();
         }
     }
-
     
     public String validateLogin(String username, String password) throws SQLException {
     	 //get connection with DB
         Connection connect = DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1:3306/test","testuser","123456");
+                "jdbc:mysql://152.3.53.14:3306/drucker","myblog","123456");
         System.out.println("Success connect Mysql server!");
         Statement stmt = connect.createStatement();
-        String sql_command = "select * from user" ;
+        String sql_command = "select * from login where username=" + "'" + username + "'";
         ResultSet rs = stmt.executeQuery(sql_command);
+        JSONObject obj = new JSONObject();
         if (rs.next()) {
-       	  System.out.println("ok");
-        String pwd = rs.getString("password");
-        if (pwd.equals(password)) {
-           // System.out.println("ok1!");
-            return "yah!";
-        } else if (!pwd.equals(password)) {
-            //System.out.println("ok2!");
-            return "wha?";
-        } else {
-           // System.out.println("ok3!");
-            return "nay!";
+       	 	String pwd = rs.getString("password");
+            //System.out.println(pwd);
+            obj.put("username", username);
+             if (pwd.equals(password)) {
+            	 obj.put("valid",new Boolean(true));
+             }
+             else {
+            	 obj.put("valid",new Boolean(false));
+             }
         }
-        }
-		return null;
+        System.out.println(obj); //this is the returned JSON object
+        return null;
     }
 }
