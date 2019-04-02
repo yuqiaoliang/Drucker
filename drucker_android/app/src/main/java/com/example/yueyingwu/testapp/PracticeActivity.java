7d78bdd5aa1e5b15636f09305c0f94f6711dd4d4
mj205@duke.edu <mj205@duke.edu>
2019-04-02 10:08:45 -0400

    add pdf view

--- drucker_android/app/src/main/java/com/example/yueyingwu/testapp/PracticeActivity.java
+++ drucker_android/app/src/main/java/com/example/yueyingwu/testapp/PracticeActivity.java
@@ -3,7 +3,10 @@ package com.example.yueyingwu.testapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class PracticeActivity extends AppCompatActivity {
@@ -12,7 +15,15 @@ public class PracticeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
-
+       // Log.i("Info","inPDFview");
+        WebView guidepdfView = (WebView)findViewById(R.id.userguideView);
+        //Log.i("Info","line20");
+        guidepdfView.getSettings().setJavaScriptEnabled(true);
+        //Log.i("Info","javascriptEnabled");
+        guidepdfView.setWebViewClient(new WebViewClient());
+        //Log.i("Info","setWebViewClient");
+        guidepdfView.loadUrl("https://docs.google.com/viewerng/viewer?url=https://trinitycommons.com/assets/files/A1-6.pdf");
+        //guidepdfView.loadUrl("https://doodle.com/poll/i9mi8mus7wxq98qg");
       /*  final Button pracBack = findViewById(R.id.pracBack);
         pracBack.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
