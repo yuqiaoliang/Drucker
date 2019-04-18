bc9d98fbf48e8389dd768b1758357cd452bcc43f
xx66 <xx66@duke.edu>
2019-04-17 23:42:29 -0400

    Message Box Model April 17

--- drucker_android/app/src/main/java/com/example/yueyingwu/testapp/MessageBoxActivity.java
+++ drucker_android/app/src/main/java/com/example/yueyingwu/testapp/MessageBoxActivity.java
@@ -6,13 +6,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MessageBoxActivity extends AppCompatActivity {
+
+  //     public void toPost(View v) {
+   //   Intent postIntent = new Intent(getApplicationContext(), PostActivity.class);
+   //    startActivity(postIntent);
+  // }
+      //public void toComment(View v) {
+      // Intent commentIntent = new Intent(getApplicationContext(), CommentActivity.class);
+      // startActivity(commentIntent);
+ //  }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_box);
+
+
+        final ImageButton iBPost=findViewById(R.id.iBPost);
+        final ImageButton iBComment=findViewById(R.id.iBComment);
+
+        iBPost.setScaleType(ImageButton.ScaleType.FIT_XY);
+        iBPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent postIntent = new Intent(getApplicationContext(), PostActivity.class);
+                startActivity(postIntent);
+            }
+        });
+        iBComment.setScaleType(ImageButton.ScaleType.FIT_XY);
+        iBComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent commentIntent = new Intent(getApplicationContext(), CommentActivity.class);
+                startActivity(commentIntent);
+            }
+        });
+
+
 
        /* final Button msgBack = findViewById(R.id.msgBack);
         msgBack.setOnClickListener(new OnClickListener(){
