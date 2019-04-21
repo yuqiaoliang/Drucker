72571fd01b143e768f14f1cc3f1a7fe3eef98b6f
xx66 <xx66@duke.edu>
2019-04-21 13:14:33 -0400

    Merge Minghui and Steve

--- drucker_android/app/src/main/java/com/example/yueyingwu/testapp/MessageBoxActivity.java
+++ drucker_android/app/src/main/java/com/example/yueyingwu/testapp/MessageBoxActivity.java
@@ -6,13 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MessageBoxActivity extends AppCompatActivity {
+  //  public void toNewPost(View v) {
+  //    Intent newPostIntent = new Intent(getApplicationContext(), NewPost.class);
+ //      startActivity(newPostIntent);
+ // }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_box);
+        final ImageButton iBNewPost = findViewById(R.id.iBNewPost);
+        final ImageButton iBViewPost = findViewById(R.id.iBViewPost);
+
+        iBNewPost.setScaleType(ImageButton.ScaleType.FIT_XY);
+        iBNewPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent newPostIntent = new Intent(getApplicationContext(), NewPost.class);
+                startActivity(newPostIntent);
+            }
+        });
+
+        iBViewPost.setScaleType(ImageButton.ScaleType.FIT_XY);
+        iBViewPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent viewPostIntent = new Intent(getApplicationContext(), ViewPostActivity.class);
+                startActivity(viewPostIntent);
+            }
+        });
+
 
        /* final Button msgBack = findViewById(R.id.msgBack);
         msgBack.setOnClickListener(new OnClickListener(){
