d5de3b7e52c271c3ef4a4fe18a06aec69fc2aa18
xx66 <xx66@duke.edu>
2019-04-23 17:18:18 -0400

    Solved the problem of Comment Dialog crash

--- drucker_android/app/src/main/java/com/example/yueyingwu/testapp/TestNewCommentActivity.java
+++ drucker_android/app/src/main/java/com/example/yueyingwu/testapp/TestNewCommentActivity.java
@@ -1,5 +1,6 @@
 package com.example.yueyingwu.testapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.AlertDialog;
@@ -30,33 +31,56 @@ import java.net.URI;
 import java.net.URL;
 import java.text.DecimalFormat;
 
-public class TestNewCommentActivity extends AppCompatActivity implements NewCommentDialog.CommentDialogListener {
-   private Button button;
-   public TextView tvNewCommentView;
+public class TestNewCommentActivity extends AppCompatActivity {
+   //private Button button;
+   //public TextView tvNewCommentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_new_comment);
-       // Button button;
-        //TextView tvNewCommentView;
-        button = (Button) findViewById(R.id.bNewComment);
+
+        final Button button = (Button) findViewById(R.id.bNewComment);
+        final TextView tvNewCommentView = findViewById(R.id.tvNewCommentView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDialog();
+                AlertDialog.Builder builder = new AlertDialog.Builder(TestNewCommentActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.new_comment_dialog, null);
+                final EditText etNewComment = (EditText)mView.findViewById(R.id.etNewComment);
+                /**
+                Button bPostComment = (Button)mView.findViewById(R.id.bPostComment);
+               /bPostComment.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tvNewCommentView.setText(etNewComment.getText().toString());
+                    }
+                });
+                //openDialog();
+                **/
+                builder.setView(mView)
+                        .setTitle("New Comment")
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("Post Comment", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                tvNewCommentView.setText(etNewComment.getText().toString());
+                                //Here you add new comment to ArrayList
+                                //CommentArraylist.add(etNewComment);
+                            }
+                        })
+                ;
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
-    }
-    public void openDialog(){
-        NewCommentDialog newCommentDialog = new NewCommentDialog();
-        newCommentDialog.show(getSupportFragmentManager(), "New Comment Dialog");
 
     }
 
-    @Override
-    public void applyTexts(String newComment) {
-        tvNewCommentView.setText(newComment);
-    }
 }
